19a6147506da16791bf5beae1a62b27fa75b20ff
pira777 <engr_pira@yahoo.com>
2026-03-28 14:03:19 +0100

    change url

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("chat/", views.chat_page, name="chat_page"),
+    path("", views.chat_page, name="chat_page"),
     path("api/chat/", views.chat_api, name="chat_api"),
 ]
